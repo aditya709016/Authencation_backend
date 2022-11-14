0c1c2164355ebf4b468827c6a01fd3345b5c47e0
aditya709016 <adityaraj.as709@gmail.com>
2022-11-15 02:13:46 +0530

    all routes are configured

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,24 +65,20 @@ class UserController {
     }
   }
 
-  static userNickname = async (req, res) => {
-    res.send({ "user": req.user })
-  }
-
   static userReset = async (req, res) => {
     const { email } = req.body
     if (email) {
       const user = await UserModel.findOne({ email: email })
       if (user) {
-        const secret = process.env.JWT_SECRET_KEY
+        const secret = user._id + process.env.JWT_SECRET_KEY
         const token = jwt.sign({ userID: user._id }, secret, { expiresIn: '15m' })
-        const link = `http://127.0.0.1:3000/password/reset/${token}`
+        const link = `http://127.0.0.1:3000/api/user/reset/${user._id}/${token}`
         console.log(link)
-         // Send Email
+        // // Send Email
         // let info = await transporter.sendMail({
         //   from: process.env.EMAIL_FROM,
         //   to: user.email,
-        //   subject: "brlASSI - Password Reset Link",
+        //   subject: "GeekShop - Password Reset Link",
         //   html: `<a href=${link}>Click Here</a> to Reset Your Password`
         // })
         res.send({ "status": "success", "message": "Password Reset Email Sent... Please Check Your Email" })
@@ -96,9 +92,9 @@ class UserController {
 
   static userPasswordReset = async (req, res) => {
     const { password, password_confirmation } = req.body
-    const {id, token } = req.params
+    const { id, token } = req.params
     const user = await UserModel.findById(id)
-    const new_secret = process.env.JWT_SECRET_KEY
+    const new_secret = user._id + process.env.JWT_SECRET_KEY
     try {
       jwt.verify(token, new_secret)
       if (password && password_confirmation) {
@@ -118,6 +114,102 @@ class UserController {
       res.send({ "status": "failed", "message": "Invalid Token" })
     }
   }
+  static change_nickname = async (req, res) => {
+      const new_nickname = req.body.new_nickname;
+      console.log(
+        await User.updateOne({ email: req.userEmail }, { nickname: new_nickname })
+      );
+      res.redirect("/user/nickname");
+      // res.status(201).json({
+      //   message: `Nickname updated to ${new_nickname}`,
+      // });
+    };
+    static makeAdmin = async (req, res) => {
+      res.redirect(`/admin/make_admin/${req.query.email}`);
+    }
+    static change_role = async (req, res) => {
+      const user_email = req.params.email;
+      const user = await user.findOne({ email: user_email });
+      const admin = await user.findOne({ _id: req.userId });
+      if (admin.role === "admin") {
+        if (user) {
+          console.log(
+            await user.updateOne({ email: user_email }, { role: "admin" }),
+            "User is authorized as an Admin"
+          );
+          res.render("success", { message: "User is authorized for admin's role" });
+    
+          
+        } else {
+          res.send({ "status": "failed", "message": "User not found"})
+        }
+      }
+    }
+    static get_nickname = async (req, res) => {
+      try {
+        const user = await User.findOne({ _id: req.userId });
+        if (user) {
+          console.log("Nickname:", user.nickname);
+          res.render("profile", {
+            name: user.nickname,
+            email: user.email,
+            role: user.role,
+          });
+          
+        } else {
+          console.log("Error finding user");
+          res.render("error", {
+            message_1: "SORRY",
+            message_2: "Error finding user",
+            brace: "(",
+          });
+          
+        }
+      } catch (err) {
+        console.log(err);
+        res.render("error", {
+          message_1: "OOPS!",
+          message_2: "Something went wrong",
+          brace: "(",
+        });
+        
+      }
+    };
+    static delete_user = async (req, res) => {
+      try {
+        const admin = await User.findOne({ _id: req.userId });
+        const user = await User.findOne({ email: req.params.email });
+        if (user) {
+          console.log(user);
+          if (admin.role === "admin") {
+            console.log(
+              await User.deleteOne({ email: req.params.email }),
+              "\n user deleted"
+            );
+            res.render("success", { message: "User Deleted" });
+    
+            
+          } else {
+            console.log("Unauthorized to delete");
+            res.render("error", {
+              message_1: "SORRY!",
+              message_2: "Not Authorized to do that",
+              brace: "(",
+            });
+            
+          }
+        }
+      } catch (err) {
+        console.log(err);
+        res.render("error", {
+          message_1: "OOPS!",
+          message_2: "Something went wrong",
+          brace: "(",
+        });
+        
+      }
+    }
+    
 }
 
 export default UserController
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,16 +4,19 @@ import UserController from '../controllers/userController.js';
 import checkUserAuth from '../middlewares/auth-middleware.js';
 
 // ROute Level Middleware - To Protect Route
-router.use('/user/nickname', checkUserAuth)
+router.use("/user/nickname", checkUserAuth)
+router.use("/user/nickname", checkUserAuth)
+router.use("/admin/make_admin/:email", checkUserAuth)
+router.use("/admin/delete/:email", checkUserAuth)
 
-// Public Routes
+
 router.post('/signup', UserController.userSignup)
 router.post('/login', UserController.userLogin)
 router.post('/password/reset', UserController.userReset)
 router.post('/password/reset/:token', UserController.userPasswordReset)
-
-// Protected Routes
-router.get('/user/nickname', UserController.userNickname)
-
+router.get("/user/nickname", UserController.get_nickname)
+router.post("/user/nickname", UserController.change_nickname)
+router.get("/admin/make_admin/:email",UserController.change_role)
+router.get("/admin/delete/:email", UserController.delete_user)
 
 export default router
